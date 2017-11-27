45c04a5c9e722ff4a444b3405154fb0b187eb1f0
kou kaicheng <18610925224@163.com>
2017-11-27 23:21:16 +0800

    初建歌手详情页面

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,57 +1,66 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail">
-    </div>
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
-
 </template>
 
 <script type="text/ecmascript-6">
-  import {mapGetters} from 'vuex'
+  import MusicList from 'components/music-list/music-list'
   import {getSingerDetail} from 'api/singer'
-  import {ERR_OK} from "api/config"
+  import {ERR_OK} from 'api/config'
+  import {createSong} from 'common/js/song'
+  import {mapGetters} from 'vuex'
 
   export default {
-    data() {
-      return {}
-    },
     computed: {
+      title() {
+        return this.singer.name
+      },
+      bgImage() {
+        return this.singer.avatar
+      },
       ...mapGetters([
         'singer'
       ])
     },
+    data() {
+      return {
+        songs: []
+      }
+    },
     created() {
-      this._getSingerDetail()
+      this._getDetail()
     },
     methods: {
-      _getSingerDetail() {
+      _getDetail() {
         if (!this.singer.id) {
-          this.$router.back()
+          this.$router.push('/singer')
           return
         }
         getSingerDetail(this.singer.id).then((res) => {
           if (res.code === ERR_OK) {
-            console.log(res.data.list)
+            this.songs = this._normalizeSongs(res.data.list)
           }
         })
+      },
+      _normalizeSongs(list) {
+        let ret = []
+        list.forEach((item) => {
+          let {musicData} = item
+          if (musicData.songid && musicData.albummid) {
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
       }
+    },
+    components: {
+      MusicList
     }
   }
 </script>
 
-
-<style lang="stylus" rel="stylesheet/stylus">
-  @import '~common/stylus/variable'
-
-  .singer-detail
-    position: fixed
-    z-index: 1000
-    top: 0
-    left 0
-    right 0
-    bottom 0
-    background $color-background
-
+<style scoped lang="stylus" rel="stylesheet/stylus">
   .slide-enter-active, .slide-leave-active
     transition: all 0.3s
 
